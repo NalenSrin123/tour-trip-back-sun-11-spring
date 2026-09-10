01fb8e1bd2d164a2706dd69631c149ff917f8d8a
trenmono <byeworld0402@gmail.com>
2026-09-10 10:53:21 +0700

    finish testing

--- src/main/java/com/etec/tourtripapi/auth/service/MailService.java
+++ src/main/java/com/etec/tourtripapi/auth/service/MailService.java
@@ -1,5 +1,8 @@
 package com.etec.tourtripapi.auth.service;
 
+import org.springframework.stereotype.Service;
+
+@Service 
 public interface MailService {
     void sendPasswordResetEmail(String recipient, String recipientName, String resetToken);
 }
